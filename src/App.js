70d51753d826ccc6ff283ed61d415f1f5f7574a6
joew2k <ogwuchejoseph25@gmail.com>
2022-11-27 18:40:56 +0100

    completed api connection

--- src/App.js
+++ src/App.js
@@ -1,25 +1,47 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from "react";
+import './App.css'
+import SearchIcon from './Search.svg'
+import MovieCard from "./MovieCard";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const API_URL = 'http://www.omdbapi.com/?i=tt3896198&apikey=7a9f9d8b'
+const App = () =>{
+
+  const [movies, setMovies] = useState([])
+  const movieSearch = async(title) => {
+  const response = await fetch(`${API_URL}&s=${title}`)//used template string
+  const data = await response.json()
+    setMovies(data.Search)
+  }
+  const [searchTerm, setSearchTerm] = useState('')
+  useEffect(() =>{
+    movieSearch('superman')
+
+  }, [])
+    return(
+        <div className="app">
+          <h1>MovieWorld</h1>
+          <div className="search">
+            <input
+            placeholder="search for movie"
+            value={searchTerm}
+            onChange={(e)=>setSearchTerm(e.target.value)}
+            />
+            <img
+            src={SearchIcon} alt="search" onClick={()=>movieSearch(searchTerm)}></img>
+          </div>
+          {
+            movies?.length>0
+            ?
+            (<div className="container">
+            {movies.map((movie)=>(
+              <MovieCard movie ={movie}/>
+            ))}
+          </div>): (
+            <div>No Movie Found</div>
+          )
+          };
+          
+        </div>
+    )
 }
-
-export default App;
+export default App
